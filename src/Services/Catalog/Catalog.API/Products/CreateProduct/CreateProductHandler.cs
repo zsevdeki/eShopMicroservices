71662dbc76421d512a9fd9cf2f6a781ee9e15b72
zskir <zskir@DESKTOP-DNP5ECO>
2025-01-27 22:38:23 +0300

    Marten connection string added

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,43 +1,42 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
-using MediatR;
+﻿
+namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct
+public record CreateProductCommand(string Name, List<string> Category ,string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+public class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public record CreateProductCommand(string Name, List<string> Category ,string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
-    public record CreateProductResult(Guid Id);
-    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        // Bussiness logic
+
+        // Create Product entity from Command object
+
+        var product = new Product
         {
-            // Bussiness logic
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
 
-            // Create Product entity from Command object
-
-            var product = new Product
-            {
-                Name = command.Name,
-                Category = command.Category,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price,
-
-            };
+        };
 
 
-            // Save to db (skip this step)
+        // Save to db (skip this step)
+
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
 
-            // return CreateProductResult as a result
+        // return CreateProductResult as a result
 
-            return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
 
 
 
 
 
-        
-        
-        }
+    
+    
     }
 }
