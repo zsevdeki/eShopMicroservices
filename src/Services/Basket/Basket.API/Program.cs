ba0179c344b7c6a9e93f1c0c69c8a8460084ddf5
zskir <zskir@DESKTOP-DNP5ECO>
2025-04-22 17:57:12 +0300

    Basket get, store, delete opr.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,7 +1,40 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using BuildingBlocks.Behaviors;
+using Carter;
+using Basket.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add Services To The Container
+// Add services to the container.
+
+//Application Services
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+
+//Data Services
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+
+
+
 var app = builder.Build();
 
-// Configure The Http Request Pipeline 
+// Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+
 app.Run();
